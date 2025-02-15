bbb9d3b648f73f415728424f67086c9878f3a4a9
yohannes-shiferaw33 <yohanneskefelegnn@gmail.com>
2025-02-15 12:51:06 -0800

    Refactored database connection

--- project/utils/create_db.py
+++ project/utils/create_db.py
@@ -2,20 +2,20 @@ import psycopg
 from config import CONNECT_DB
 
 async def create_db():
-    connect=f"host=127.0.0.1 "\
-            f"port=5432 "\
-            f"user=postgres "\
-            f"password=1234567 "\
-            f"connect_timeout=10"
-    conn=psycopg.connect(connect)
-    conn.autocommit=True
-    curs=conn.cursor()
-    try:
-        curs.execute(f'CREATE DATABASE info WITH OWNER=postgres ENCODING=\'UTF8\'')
-    except Exception as e:
-        pass
-    curs.close()
-    conn.close()
+    # connect=f"host=127.0.0.1 "\
+    #         f"port=5432 "\
+    #         f"user=postgres "\
+    #         f"password=1234567 "\
+    #         f"connect_timeout=10"
+    # conn=psycopg.connect(connect)
+    # conn.autocommit=True
+    # curs=conn.cursor()
+    # try:
+    #     curs.execute(f'CREATE DATABASE info WITH OWNER=postgres ENCODING=\'UTF8\'')
+    # except Exception as e:
+    #     pass
+    # curs.close()
+    # conn.close()
     async with await psycopg.AsyncConnection.connect(CONNECT_DB) as conn:
         async with conn.cursor() as curs:
             await curs.execute(
